dfa3d3c1c3b13ce488989a90e3084a6b0de9c260
Jade Van Horn <jade@elephantenergy.com>
2023-12-07 15:14:57 -0700

    import_stripe.py

--- import_stripe.py
+++ import_stripe.py
@@ -1,6 +1,6 @@
-Stripe.apiKey = "sk_test_51Itf0iBfPYcjROShi56UtBWV7LSOL8NXIdOBIuomUYsBURIBIVTLJDaytHADabDx4O60w0HNEr68Vc0sdbA6Y9Jr00lH37G4R6";
+Stripe.apiKey = "pk_live_51Itf0iBfPYcjROShoTxAJMuKSiALPmBi1MgtLxub6UkXZ2Sos884Sy6E0R221dIQyT5ZFDi5ywz4kIKMxmIdag2c00GmcepI6B";
 
 ProductCreateParams params =
-  ProductCreateParams.builder().setName("Gold Plan").build();
+  ProductCreateParams.builder().setName(name).build();
 
 Product product = Product.create(params);
